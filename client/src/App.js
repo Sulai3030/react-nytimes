e0647af7677bab5fc88fc5edf36c942f289d9e37
sully34 <sssulai@gmail.com>
2018-11-15 16:06:17 -0500

    fixeds routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,22 @@
 import React from "react";
-import {BrowserRouter as Router, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Wrapper from "./components/Wrapper";
-import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Books from "./pages/Books";
+import Detail from "./pages/Detail";
+import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
 
-const App = () =>
+const App = () => (
   <Router>
-      <div>
-        <Navbar />
-        <Wrapper>
-        </Wrapper>
-        <Footer />
-     </div>
+    <div>
+      <Nav />
+      <Switch>
+        <Route exact path="/" component={Books} />
+        <Route exact path="/books" component={Books} />
+        <Route exact path="/books/:id" component={Detail} />
+        <Route component={NoMatch} />
+      </Switch>
+    </div>
   </Router>
+);
+
 export default App;
--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
-
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const routes = require('./routes');
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -10,7 +12,18 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+// Add routes, both API and view
+app.use(routes);
 
+// Set up promises with mongoose
+mongoose.Promise = global.Promise;
+// Connect to the Mongo DB (react reading list)
+mongoose.connect(
+ process.env.MONGODB_URI || 'mongodb://localhost/reactreadinglist',
+ {
+ //  useMongoClient: true
+ }
+);
 // Define API routes here
 
 // Send every other request to the React app
